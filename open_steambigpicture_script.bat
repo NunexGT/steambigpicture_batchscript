b28cbf10ea6065febd7222e7b5a7b33a813b852e
nunex <nunosantos12350@gmail.com>
2022-12-12 19:15:12 +0000

    Create open_steambigpicture_script.bat

---
+++ open_steambigpicture_script.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDVBQgvCEWK9E7AQ1Mz+/OGTrV0hZ/Y6eYKb+L2JYNAa6VHxZ5MRxW5fmcYOBRwWewquDg==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+echo "Made by SuperX-dev on Github ;)" & echo "Checking if Steam is Open and closing it, if it is already..." & taskkill /f /IM "steam.exe" & taskkill /f /IM "SteamService.exe" & taskkill /f /IM "steamwebhelper.exe" & echo "All Steam processes closed!" & echo "Starting Steam Big Picture Mode..." & "C:\Program Files (x86)\Steam\Steam.exe" -bigpicture & exit
